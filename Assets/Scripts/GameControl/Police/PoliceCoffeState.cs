1707f412e2ec243156dafa0cc406510112ac5434
aidan197 <aidan_yepson@hotmail.com>
2020-10-07 16:57:49 -0500

    police completo
    police completo

---
+++ Assets/Scripts/GameControl/Police/PoliceCoffeState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceCoffeState :PoliceBaseState<Police>
+{
+    string stateName = "PoliceCoffeState";
+
+    public override void Enter(Police charac)
+    {
+        charac.ResetProperties();
+        charac.TargetSeek = charac.FindClosestCofee();
+        charac.OnSeek = true;
+    }
+    public override void Execute(Police charac)
+    {
+      
+    }
+
+       public override void Exit(Police charac)
+    {   
+        charac.currentState = 4;
+        charac.ResetProperties();
+    }
+
+}
